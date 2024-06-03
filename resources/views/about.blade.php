c4ea4344e509badcd77a036a5bb8f0218245b0dc
markejumali123 <markejumali123@gmail.com>
2024-06-03 22:33:14 +0800

    umali

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,7 +6,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">{{ __('About us') }}</h1>
+                    <h1 class="m-0">{{ __('HEllO MAAM YUMI KHO') }}</h1>
                 </div><!-- /.col -->
             </div><!-- /.row -->
         </div><!-- /.container-fluid -->
@@ -19,13 +19,7 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ __('About us') }}</h5>
-
-                            <p class="card-text">
-                                {{ __('Sample static text page ') }}
-                            </p>
-                        </div>
+                       
                     </div>
                 </div>
             </div>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -39,6 +39,15 @@
             </li>
 
             <li class="nav-item">
+                <a href="{{ route('about') }}" class="nav-link">
+                    <i class="nav-icon fas fa-users"></i>
+                    <p>
+                        {{ __('Joel-jay-markej') }}
+                    </p>
+                </a>
+            </li>
+
+            <li class="nav-item">
                 <a href="#" class="nav-link">
                     <i class="nav-icon fas fa-circle nav-icon"></i>
                     <p>
