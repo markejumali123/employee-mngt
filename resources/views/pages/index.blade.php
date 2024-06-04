6f2e4e00ef1229fa6698dcd400f1ac6a97bc23a4
Joel De Jesus <dejesus07021@gmail.com>
2024-06-04 10:18:06 +0800

    hi

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
-        <h3 align="center" class="mt-5">Employee Register</h3>
+        <h3 align="center" class="mt-5">Employee Registration</h3>
         <div class="row">
             <div class="col-md-2">
             </div>
